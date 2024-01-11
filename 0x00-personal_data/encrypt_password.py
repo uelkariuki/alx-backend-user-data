eba7d5d60bded6ec7f5ac1077cd8a2eaabb5becc
uelkariuki <cratzuel@gmail.com>
2024-01-12 01:39:06 +0300

    Implement a hash_password function that returns a salted, hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,12 +7,10 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-	"""
-	Returns a salted, hashed password, which is a byte string
-	"""
-	Bytes = password.encode('utf-8')
-	salt = bcrypt.gensalt()
-	hash = bcrypt.hashpw(Bytes, salt)
-	return hash
-
-
+    """
+    Returns a salted, hashed password, which is a byte string
+    """
+    Bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(Bytes, salt)
+    return hashed_password
