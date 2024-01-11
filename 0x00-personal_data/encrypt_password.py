83aacb08a204a94138277679839aa1ece33720d1
uelkariuki <cratzuel@gmail.com>
2024-01-12 01:57:07 +0300

    Implement an is_valid function to validate that the provided password matches the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,13 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(Bytes, salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password
+    """
+    password_bytes = password.encode('utf-8')
+    result = bcrypt.checkpw(password_bytes, hashed_password)
+
+    return result
