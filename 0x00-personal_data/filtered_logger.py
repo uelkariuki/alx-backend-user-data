4d1582d8dcf9d1a45a689976bacc38dc1416bf86
uelkariuki <cratzuel@gmail.com>
2024-01-11 11:17:08 +0300

    Add function filter_datum that returns the log message obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,17 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
-    """ Return: the log message obfuscated """
+                 separator: str) -> str:
+    """
+    Return: the log message obfuscated
+    Arguments:
+    fields: a list of strings representing all fields to obfuscate
+    redaction: a string representing by what the field will be obfuscated
+    message: a string representing the log line
+    separator: a string representing by which character is separating all
+    fields in the log line (message)
+    """
     for field in fields:
-            message = re.sub(f"{field}=[^{separator}]*", f"{field}={redaction}", message)
+            message = re.sub(f"{field}=[^{separator}]*",
+                             f"{field}={redaction}", message)
     return message
