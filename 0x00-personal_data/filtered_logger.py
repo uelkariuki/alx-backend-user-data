f2d0cd949f302738fee6be8aa635614d13320aab
uelkariuki <122826998+uelkariuki@users.noreply.github.com>
2024-01-14 09:59:21 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,7 +62,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def get_db() -> connection.MySQLConnection:
     """
     Returns a connector to the database (mysql.connector.
     connection.MySQLConnection object
