f37b765e9645a72452db7bfcd5e9552f3042c05f
uelkariuki <cratzuel@gmail.com>
2024-01-16 20:52:13 +0300

    Add method that returns the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 Basic auth class that inherits from Auth
 """
 
+import base64
+import binascii
 from flask import request
 from api.v1.auth.auth import Auth
 
@@ -24,3 +26,35 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split('Basic ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        base64_authorization_header = check_if_valid_base64(
+            base64_authorization_header)
+        if base64_authorization_header is None:
+            return None
+
+        base64string = base64_authorization_header.encode().decode('utf-8')
+        decoded_bytes = base64.b64decode(base64string)
+        decoded_string = decoded_bytes.decode('utf-8')
+        return decoded_string
+
+
+def check_if_valid_base64(base64_authorization_header):
+    """ Checks if header is a valid base64 string
+    """
+    try:
+        base64.b64decode(base64_authorization_header)
+        return base64_authorization_header
+    except binascii.Error:
+        # if error is raised it is not a valid Base64 String
+        return None
