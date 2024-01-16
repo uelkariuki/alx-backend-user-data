3032a6b5ddb7852724923fc85d39d6600c1a2f21
uelkariuki <cratzuel@gmail.com>
2024-01-16 18:27:33 +0300

    Implement Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Basic auth class that inherits from Auth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Class BasicAuth that inherits from Auth"""
+    pass
