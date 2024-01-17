1f0bebbabd852989eb8717c749159dd8a289baca
uelkariuki <cratzuel@gmail.com>
2024-01-17 04:06:36 +0300

    Improve require_auth by allowing * at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,7 @@ class Auth:
                                                 excluded_path[:-1]):
                 return False
 
-        if path in excluded_paths:
+        if path == excluded_path:
             return False
         return True
 
