c72274f0e87106ce15f86ef6c1e2059c501cad6b
uelkariuki <cratzuel@gmail.com>
2024-01-18 07:08:08 +0300

    Add a class SessionAuth and create an empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" class SessionAuth
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class SessionAuth that inherits from Auth
+    """
+    pass
