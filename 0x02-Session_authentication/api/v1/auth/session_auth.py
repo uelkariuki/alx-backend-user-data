e076f4e6239b1c82dd499589080b391784367fc5
uelkariuki <cratzuel@gmail.com>
2024-01-18 11:24:08 +0300

    Add an instance method that returns a User instance based on a cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,6 +6,8 @@
 from api.v1.auth.auth import Auth
 import uuid
 
+from models.user import User
+
 
 class SessionAuth(Auth):
     """ Class SessionAuth that inherits from Auth
@@ -31,3 +33,11 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Returns a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
