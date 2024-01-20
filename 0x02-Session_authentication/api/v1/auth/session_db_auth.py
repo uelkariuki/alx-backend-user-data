daeefecd1cf5f1e07f6e7910fd5ea1a35ad66191
uelkariuki <cratzuel@gmail.com>
2024-01-20 17:29:38 +0300

    Create a new authentication system, based on Session ID stored in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -12,7 +12,9 @@ class SessionDBAuth(SessionExpAuth):
     class SessionDBAuth
     """
     def create_session(self, user_id=None):
-        """ create session
+        """
+        Creates and stores new instance of UserSession and returns the
+        Session ID
         """
         session_id = super().create_session(user_id)
         if not session_id:
@@ -27,6 +29,8 @@ class SessionDBAuth(SessionExpAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """ overload user_id_for_session_id
+        Returns the User ID by requesting UserSession in the database
+        based on session_id
         """
         user_id = UserSession.search({'session_id': session_id})
         if user_id:
@@ -35,6 +39,8 @@ class SessionDBAuth(SessionExpAuth):
 
     def destroy_session(self, request=None):
         """ overload destroy session
+        Destroys the UserSession based on the Session ID from the request
+        cookie
         """
         if request is None:
             return False
