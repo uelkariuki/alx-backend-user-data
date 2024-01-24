d9f541394d8b4e21d8ce8d6d4f08c9ca48756386
uelkariuki <cratzuel@gmail.com>
2024-01-24 21:33:57 +0300

    Implement Auth.destroy_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -96,7 +96,7 @@ class Auth:
         user = self._db.find_user_by(user_id=user_id)
         if user:
             self._db.update_user(user.id, session_id=None)
-
+			return None
 
 def _generate_uuid() -> str:
     """
