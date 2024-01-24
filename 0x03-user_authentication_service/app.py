efb1d18a0feac4e0c9a4e9cb1a484f08461899dc
uelkariuki <cratzuel@gmail.com>
2024-01-25 01:50:18 +0300

    Implement the update_password function to respond to the PUT /reset_password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -99,7 +99,8 @@ def update_password():
         new_password = request.form.get('new_password')
 
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
+        return jsonify({"email": f'{email}',
+                        "message": "Password updated"}), 200
     except Exception:
         abort(403)
 
