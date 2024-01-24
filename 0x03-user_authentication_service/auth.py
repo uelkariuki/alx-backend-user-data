115aae5a436a47757b79b02a5dcfc4aad7880316
uelkariuki <cratzuel@gmail.com>
2024-01-24 19:41:04 +0300

    Implement the Auth.create_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,6 +62,20 @@ class Auth:
         except Exception:
             return False
 
+    def create_session(self, email: str) -> str:
+        """
+        create session
+        """
+
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """
