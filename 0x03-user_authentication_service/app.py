d66355380cca9669b2dd14dbad7c11f4b26ad967
uelkariuki <cratzuel@gmail.com>
2024-01-25 02:18:12 +0300

    Fix exception to except ValueError

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -98,7 +98,7 @@ def update_password() -> str:
     new_password = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_password)
-    except Exception:
+    except ValueError:
         abort(403)
     return jsonify({"email": user_email, "message": "Password updated"}), 200
 
